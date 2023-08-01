1191fa316afb5bebc66ba9f995a18f53a66d1a87
mikiii24 <mikibask24@gmail.com>
2023-08-01 17:39:12 +0900

    バリデーションとマイグレーションファイルの記述

--- db/migrate/20230801072942_create_items.rb
+++ db/migrate/20230801072942_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      
+      t.string :name,                    null: false, default: ""
+      t.text :item_info,                 null: false, default: nil
+      t.integer :category_id,            null: false, default: nil
+      t.integer :sales_status_id,        null: false, default: nil
+      t.integer :shipping_fee_status_id, null: false, default: nil
+      t.integer :prefecture_id,          null: false, default: nil
+      t.integer :scheduled_delivery_id,  null: false, default: nil
+      t.integer :price,                  null: false, default: nil
+      t.references :user,                null: false, foreign_key: true
       t.timestamps
     end
   end
