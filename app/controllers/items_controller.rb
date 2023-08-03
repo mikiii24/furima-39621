7a79bf676a562ef7ff09bd7a0916f11c62637df3
mikiii24 <mikibask24@gmail.com>
2023-08-03 15:28:59 +0900

    issue6_Rubocopの自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
     @shipping_fee_statuses = ShippingFeeStatus.all
   end
 
