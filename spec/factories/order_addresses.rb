6cdf56ebb6dfebc02cf0113bdaf970c765a6849e
mikiii24 <mikibask24@gmail.com>
2023-08-07 16:33:31 +0900

    order_addressのFactorybotの記述

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    item_id { 1 }
+    user_id { 1 }
+    postal_code { Faker::Number.between(from: 100, to: 999).to_s + "-" + Faker::Number.between(from: 1000, to: 9999).to_s }
+    shipping_prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    city { Faker::Address.city }
+    addresses { "青山1-1-1" }
+    building { "柳ビル103" }
+    phone_number { "0" + Faker::Number.between(from: 100000000, to: 9999999999).to_s }
+  end
+end
