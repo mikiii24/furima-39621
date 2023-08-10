85c47a42e45f9174b9858fa44890b1d2655322ec
mikiii24 <mikibask24@gmail.com>
2023-08-10 13:15:03 +0900

    orderコントローラーの記述の変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,16 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :move_to_root_path
+  before_action :find_params only: [:index, :create]
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @item = Item.find(params[:item_id])
     @shipping_fee_statuses = ShippingFeeStatus.all
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_address_params)
-    @item = Item.find(params[:item_id])
     if @order_address.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
@@ -32,6 +31,10 @@ class OrdersController < ApplicationController
   private
   def order_address_params
     params.require(:order_address).permit(:postal_code, :shipping_prefecture_id, :city, :addresses, :building, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def find_params
+    @item = Item.find(params[:item_id])
   end
   
   def move_to_root_path
