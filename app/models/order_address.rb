698848e2fb773e9cb29156d1a442cc1105b2d9be
mikiii24 <mikibask24@gmail.com>
2023-08-10 13:14:31 +0900

    order_addressモデルでのwith_optionsの使用

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,14 +3,18 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :token, :item_id, :user_id, :postal_code, :shipping_prefecture_id, :city, :addresses, :building, :phone_number
 
-  validates :token, presence: true
-  validates :item_id, presence: true
-  validates :user_id, presence: true
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :shipping_prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
-  validates :city, presence: true
-  validates :addresses, presence: true
-  validates :phone_number, presence: true, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
+  with_options presence: true do
+    validates :token
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :shipping_prefecture_id,numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :addresses
+    validates :phone_number
+  end
+
+  validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
   validates :phone_number, length: { minimum: 10, message: 'is too short'}
   validates :phone_number, length: { maximum: 11, message: 'is too long'}
 
