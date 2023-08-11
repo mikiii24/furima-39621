358bf4e5cfbc553f032897498fb99d1708e5c094
mikiii24 <mikibask24@gmail.com>
2023-08-11 11:09:25 +0900

    エラーメッセージの重複の解消

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,7 +7,6 @@ class OrderAddress
     validates :item_id
     validates :user_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :shipping_prefecture_id
     validates :city
     validates :addresses
     validates :phone_number
